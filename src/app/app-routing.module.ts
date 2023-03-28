bbb4ea4eb9b2e517656cca9434be8d8dbb35b1cc
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2023-03-28 13:29:32 +0200

    Routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './auth/auth.guard';
+import { CoachListComponent } from './coach-list/coach-list.component';
+import { ContactComponent } from './contact/contact.component';
+import { RegisterCoachComponent } from './register-coach/register-coach.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent, canActivate: [AuthGuard] }
+  { path: '', component: AppComponent},
+  { path: 'contact', component: ContactComponent},
+  { path: 'register', component: RegisterCoachComponent},
+  { path: 'coaches', component: CoachListComponent}
 ];
 
 @NgModule({
--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
-  { path: 'auth/login', component: LoginComponent }
+  { path: 'auth/login', component: LoginComponent },
 ];
 
 @NgModule({
