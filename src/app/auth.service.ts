af71d00cf777866210f8d0833761d64e2b9bc9bf
WhereIsYourLuck <48730787+WhereIsYourLuck@users.noreply.github.com>
2023-03-28 14:11:25 +0200

    Login

--- src/app/auth.service.ts
+++ src/app/auth.service.ts
@@ -1,16 +1,21 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Coach } from 'src/classes/Coach';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-
-  constructor() { }
-
+  
   private token!: string;
+  constructor(private http : HttpClient) { }
+
+  
 
   login() {
     this.token = 'MyFakeToken';
+    this.http.get<Coach[]>('http://localhost:3000/coach')
+    .subscribe(res => { const user = res.find((a: Coach)=>{ a.password == this.token && a.login == this.token })});
   }
 
   getToken(): string {
