815083d4dea959fa01e7750b2469123667744628
Jean-Baptiste <jbropp@gmail.com>
2023-03-28 13:45:22 +0200

    patch jb

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,15 @@ import { AuthGuard } from './auth/auth.guard';
 import { CoachListComponent } from './coach-list/coach-list.component';
 import { ContactComponent } from './contact/contact.component';
 import { RegisterCoachComponent } from './register-coach/register-coach.component';
+import { LoginComponent } from './auth/components/login/login.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
-  { path: '', component: AppComponent},
+  { path: '', component: HomeComponent},
   { path: 'contact', component: ContactComponent},
   { path: 'register', component: RegisterCoachComponent},
-  { path: 'coaches', component: CoachListComponent}
+  { path: 'coaches', component: CoachListComponent},
+  { path: 'login', component: LoginComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
